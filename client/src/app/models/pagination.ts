f83e1127347935f3d2c8845e4472967ae6429832
ChrispyBro2113 <cobz1913@gmail.com>
2021-09-20 19:52:26 -0500

    End of section 8

---
+++ client/src/app/models/pagination.ts
@@ -0,0 +1,12 @@
+import { IProduct } from "./products";
+
+export interface IPagination {
+
+        pageIndex: number;
+        pageSize: number;
+        count: number;
+        data: IProduct[];
+}
+
+
+
